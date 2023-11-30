72ce356cc801e713a07dca28945d120c49002443
george-tg <georgebahadi02@hotmail.com>
2023-11-30 16:50:53 +0100

    network part working full app blabla

--- app/src/main/java/mobappdev/example/apiapplication/data/WeatherStorage.kt
+++ app/src/main/java/mobappdev/example/apiapplication/data/WeatherStorage.kt
@@ -1,28 +1,72 @@
 package mobappdev.example.apiapplication.data
 import android.content.Context
-import android.content.SharedPreferences
-import androidx.core.content.edit
-import mobappdev.example.apiapplication.utils.localDateToString
-import java.time.LocalDate
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+
 
 object WeatherStorage {
-    private const val PREFS_NAME = "JokePrefs"
-    private const val KEY_JOKE = "saved_joke"
-    private const val KEY_LASTUPDATE = "last_update"
+    private const val PREFS_NAME = "WeatherPrefs"
+    private const val KEY_WEATHER = "saved_weather"
 
-    fun saveWeather(context: Context, weather: WeatherDaily, date: LocalDate) {
-        val prefs: SharedPreferences = context.getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE)
-        prefs.edit {
-            putString(KEY_JOKE, weather.timezone)
-            putString(KEY_LASTUPDATE, localDateToString(date))
-        }
+
+
+
+
+    fun saveWeather(context: Context, weatherData: WeatherDaily?) {
+        val prefs = context.getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE)
+        val editor = prefs.edit()
+        val gson = Gson()
+        val json = gson.toJson(weatherData)
+        editor.putString(KEY_WEATHER, json)
+        editor.apply()
     }
 
-    fun getSavedWeather(context: Context): Array<String?> {
-        val prefs: SharedPreferences = context.getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE)
-        return arrayOf(
-            prefs.getString(KEY_JOKE, null),
-            prefs.getString(KEY_LASTUPDATE, null)
-        )
+    fun loadWeather(context: Context): WeatherDaily? {
+        val prefs = context.getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE)
+        val gson = Gson()
+        val json = prefs.getString(KEY_WEATHER, null)
+        return gson.fromJson(json, object : TypeToken<WeatherDaily?>() {}.type)
     }
+
+    private const val PREFS_NAME_DETAILS = "WeatherDetailsPrefs"
+    private const val KEY_WEATHER_DETAILS = "savedWeatherDetails"
+
+    fun saveWeatherDetails(context: Context, weatherDetails: WeatherDetails?) {
+        val prefs = context.getSharedPreferences(PREFS_NAME_DETAILS, Context.MODE_PRIVATE)
+        val editor = prefs.edit()
+        val gson = Gson()
+        val json = gson.toJson(weatherDetails)
+        editor.putString(KEY_WEATHER_DETAILS, json)
+        editor.apply()
+    }
+
+    fun loadWeatherDetails(context: Context): WeatherDetails? {
+        val prefs = context.getSharedPreferences(PREFS_NAME_DETAILS, Context.MODE_PRIVATE)
+        val gson = Gson()
+        val json = prefs.getString(KEY_WEATHER_DETAILS, null)
+        return gson.fromJson(json, object : TypeToken<WeatherDetails?>() {}.type)
+    }
+
+    private const val PREFS_NAME_CURRENT = "WeatherCurrentPrefs"
+    private const val KEY_WEATHER_CURRENT = "savedWeatherCurrent"
+
+    fun saveWeatherCurrent(context: Context, weatherCurrent: WeatherCurrent?) {
+        val prefs = context.getSharedPreferences(PREFS_NAME_CURRENT, Context.MODE_PRIVATE)
+        val editor = prefs.edit()
+        val gson = Gson()
+        val json = gson.toJson(weatherCurrent)
+        editor.putString(KEY_WEATHER_CURRENT, json)
+        editor.apply()
+    }
+
+    fun loadWeatherCurrent(context: Context): WeatherCurrent? {
+        val prefs = context.getSharedPreferences(PREFS_NAME_CURRENT, Context.MODE_PRIVATE)
+        val gson = Gson()
+        val json = prefs.getString(KEY_WEATHER_CURRENT, null)
+        return gson.fromJson(json, object : TypeToken<WeatherCurrent?>() {}.type)
+    }
+
+
+
+
 }
